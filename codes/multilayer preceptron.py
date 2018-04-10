e2b5732c3fd4ea19e05993ea3d319c38b63b01f8
yufeng <yufeng11235@gmail.com>
2018-04-11 00:46:41 +0800

    add new python file "multilayer preceptron"

---
+++ codes/multilayer preceptron.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+import numpy as np
+import projectq
+from projectq.ops import H, X, Y, Measure, Ry, Rz
+import pickle
+from projectq.meta import Control
+import matplotlib.pyplot as plt
+
+# To be continued。。。。。
